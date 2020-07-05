1909e36d1d17f6e68e66fd5e8a6fb92de0c79fb3
blowin <zk.,k.fk`ye19>
2020-07-05 15:42:29 +0300

    add IdentityGuid

---
+++ Blowin.Identity/IdentityGuid.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Blowin
+{
+    public static class IdentityGuid
+    {
+        public static NullableIdentityGuid<T> Nullable<T>(Guid? id) => (NullableIdentityGuid<T>)id;
+        public static NullableIdentityGuid<T> Nullable<T>(Guid id) => (NullableIdentityGuid<T>)id;
+
+        public static IdentityGuid<T> NonNullable<T>(Guid? id) => (IdentityGuid<T>)id;
+        public static IdentityGuid<T> NonNullable<T>(Guid id) => (IdentityGuid<T>)id;
+
+        public static IdentityGuid<T> NonNullable<T>(NullableIdentityGuid<T> identityInt) => (IdentityGuid<T>)identityInt;
+    }
+    
+    public readonly struct IdentityGuid<T> : IEquatable<IdentityGuid<T>>, IEquatable<NullableIdentityGuid<T>>
+    {
+        public Guid Id { get; }
+
+        public IdentityGuid(Guid id) => Id = id;
+
+        public bool Equals(IdentityGuid<T> other) => Id == other.Id;
+        
+        public bool Equals(NullableIdentityGuid<T> other) => Id == other.Id;
+
+        public override bool Equals(object obj) => obj is IdentityGuid<T> other && Equals(other);
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public override string ToString() => Id.ToString();
+
+        #region Cast
+
+        public static implicit operator Guid(IdentityGuid<T> self) => self.Id;
+        
+        public static implicit operator Guid?(IdentityGuid<T> self) => self.Id;
+
+        public static explicit operator IdentityGuid<T>(Guid id) => new IdentityGuid<T>(id);
+        
+        public static explicit operator IdentityGuid<T>(Guid? id) => new IdentityGuid<T>(id.Value);
+
+        #endregion
+
+        public NullableIdentityGuid<T> AsNullableIdentity() => this;
+    }
+
+    public readonly struct NullableIdentityGuid<T> : IEquatable<NullableIdentityGuid<T>>, IEquatable<IdentityGuid<T>>
+    {
+        public Guid? Id { get; }
+
+        public bool HasId => Id.HasValue;
+
+        public Guid UnwrapId => Id.Value;
+
+        public NullableIdentityGuid(Guid? id) => Id = id;
+
+        public Guid ValueOr(Guid defaultId = default) => Id ?? defaultId;
+
+        public Guid ValueOr(Func<Guid> defaultIdFactory)
+        {
+            if(defaultIdFactory == null)
+                throw new ArgumentNullException(nameof(defaultIdFactory));
+
+            return Id ?? defaultIdFactory();
+        }
+
+        public bool Equals(NullableIdentityGuid<T> other) => Id == other.Id;
+        public bool Equals(IdentityGuid<T> other) => Id == other.Id;
+
+        public override bool Equals(object obj) => obj is NullableIdentityGuid<T> other && Equals(other);
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public override string ToString() => Id.HasValue ? Id.ToString() : "NULL";
+
+        public IdentityGuid<T> AsIdentityOrThrow() => (IdentityGuid<T>)this;
+
+        #region Cast
+
+        public static implicit operator NullableIdentityGuid<T>(IdentityGuid<T> self) => new NullableIdentityGuid<T>(self.Id);
+
+        public static implicit operator Guid?(NullableIdentityGuid<T> self) => self.Id;
+
+        public static explicit operator NullableIdentityGuid<T>(Guid? id) => new NullableIdentityGuid<T>(id);
+        
+        public static explicit operator NullableIdentityGuid<T>(Guid id) => new NullableIdentityGuid<T>(id);
+        
+        public static explicit operator Guid(NullableIdentityGuid<T> self) => self.UnwrapId;
+
+        public static explicit operator IdentityGuid<T>(NullableIdentityGuid<T> self) => new IdentityGuid<T>(self.UnwrapId);
+
+        #endregion
+    }
+}
---
+++ Blowin.Identity/IdentityInt.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Blowin
+{
+    public static class IdentityInt
+    {
+        public static NullableIdentityInt<T> Nullable<T>(int? id) => (NullableIdentityInt<T>)id;
+        public static NullableIdentityInt<T> Nullable<T>(int id) => (NullableIdentityInt<T>)id;
+
+        public static IdentityInt<T> NonNullable<T>(int? id) => (IdentityInt<T>)id;
+        public static IdentityInt<T> NonNullable<T>(int id) => (IdentityInt<T>)id;
+
+        public static IdentityInt<T> NonNullable<T>(NullableIdentityInt<T> identityInt) => (IdentityInt<T>)identityInt;
+    }
+    
+    public readonly struct IdentityInt<T> : IEquatable<IdentityInt<T>>, IEquatable<NullableIdentityInt<T>>
+    {
+        public int Id { get; }
+
+        public IdentityInt(int id) => Id = id;
+
+        public bool Equals(IdentityInt<T> other) => Id == other.Id;
+        
+        public bool Equals(NullableIdentityInt<T> other) => Id == other.Id;
+
+        public override bool Equals(object obj) => obj is IdentityInt<T> other && Equals(other);
+
+        public override int GetHashCode() => Id;
+
+        public override string ToString() => Id.ToString();
+
+        #region Cast
+
+        public static implicit operator int(IdentityInt<T> self) => self.Id;
+        
+        public static implicit operator int?(IdentityInt<T> self) => self.Id;
+
+        public static explicit operator IdentityInt<T>(int id) => new IdentityInt<T>(id);
+        
+        public static explicit operator IdentityInt<T>(int? id) => new IdentityInt<T>(id.Value);
+
+        #endregion
+
+        public NullableIdentityInt<T> AsNullableIdentity() => this;
+    }
+
+    public readonly struct NullableIdentityInt<T> : IEquatable<NullableIdentityInt<T>>, IEquatable<IdentityInt<T>>
+    {
+        public int? Id { get; }
+
+        public bool HasId => Id.HasValue;
+
+        public int UnwrapId => Id.Value;
+
+        public NullableIdentityInt(int? id) => Id = id;
+
+        public int ValueOr(int defaultId = default) => Id ?? defaultId;
+
+        public int ValueOr(Func<int> defaultIdFactory)
+        {
+            if(defaultIdFactory == null)
+                throw new ArgumentNullException(nameof(defaultIdFactory));
+
+            return Id ?? defaultIdFactory();
+        }
+
+        public bool Equals(NullableIdentityInt<T> other) => Id == other.Id;
+        public bool Equals(IdentityInt<T> other) => Id == other.Id;
+
+        public override bool Equals(object obj) => obj is NullableIdentityInt<T> other && Equals(other);
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public override string ToString() => Id.HasValue ? Id.ToString() : "NULL";
+
+        public IdentityInt<T> AsIdentityOrThrow() => (IdentityInt<T>)this;
+
+        #region Cast
+
+        public static implicit operator NullableIdentityInt<T>(IdentityInt<T> self) => new NullableIdentityInt<T>(self.Id);
+
+        public static implicit operator int?(NullableIdentityInt<T> self) => self.Id;
+
+        public static explicit operator NullableIdentityInt<T>(int? id) => new NullableIdentityInt<T>(id);
+        
+        public static explicit operator NullableIdentityInt<T>(int id) => new NullableIdentityInt<T>(id);
+        
+        public static explicit operator int(NullableIdentityInt<T> self) => self.UnwrapId;
+
+        public static explicit operator IdentityInt<T>(NullableIdentityInt<T> self) => new IdentityInt<T>(self.UnwrapId);
+
+        #endregion
+    }
+}
